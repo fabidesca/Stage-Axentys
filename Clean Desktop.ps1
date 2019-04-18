b3444d5175d9953273be9020766f697e7fa9e51c
fabidesca <fabiandescampe@hotmail.com>
2019-04-18 15:05:18 +0200

    Add files via upload

---
+++ Clean Desktop.ps1
@@ -0,0 +1,14 @@
+﻿$path3 = "\Desktop\Microsoft Teams.lnk"
+$path2 = $env:USERPROFILE 
+$path = $path2 + $path3
+$condition = Test-Path $path
+If ($condition -eq $True){
+Remove-Item -path $path
+}
+$path3 = "\Desktop\Microsoft Edge.lnk"
+$path = $path2 + $path3
+$condition = Test-Path $path
+If ($condition -eq $True){
+Remove-Item -path $path
+}
+Get-ChildItem -Path C:\Users\Public\Desktop -Include * -File -Recurse | foreach { $_.Delete()} 
